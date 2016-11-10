b6930d966699d8c41e6d9b600b5678dac8811fd2
Mason Smith <masonium@gmail.com>
2016-11-09 21:44:06 -0800

    sample documentation in katex

--- src/eigenvalues/mod.rs
+++ src/eigenvalues/mod.rs
@@ -1,6 +1,11 @@
 //! Contains methods for solving eigenvalues, including general and
 //! symmetric/Hermitian eigenvalue problems.
 //!
+//! The eigenvalue problem is to find solution $\left(x, \lambda\right)$ to the problem:
+//!
+//! $$A \cdot x = \lambda \cdot x$$
+//!
+//! for a square matrix \\(A\\).
 
 pub mod general;
 pub mod symmetric;
